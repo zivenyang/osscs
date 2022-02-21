2143eaf70a17b4af95de5d10c2c82e78672f7239
杨子文 <yangziwen1994@gmail.com>
2022-02-21 20:21:08 +0800

    [#5] Fix code scanning alert - Full server-side request forgery

--- osscs/const/const.py
+++ osscs/const/const.py
@@ -1,10 +1,5 @@
 from urllib import parse
 
-MAVEN = 'maven'
-PYPI = 'pypi'
-GO = 'go'
-NPM = 'npm'
-
 MVN_REPOSITORY_URL = 'https://mvnrepository.com'
 MVN_REPOSITORY_PACKAGE_SEARCH_URL = parse.urljoin(MVN_REPOSITORY_URL, '/search')
 PYPI_REPOSITORY_URL = 'https://pypi.org'
