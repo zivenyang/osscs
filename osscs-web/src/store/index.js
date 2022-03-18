7a1500e2718c898d9fa9e5b369117dc850804767
杨子文 <yangziwen1994@gmail.com>
2022-03-19 01:42:04 +0800

    [#17] 完成登录、注册、用户详情前台页面初版

--- osscs-web/src/store/index.js
+++ osscs-web/src/store/index.js
@@ -1,62 +1,8 @@
-import { createStore } from 'vuex'
-import { LOGGED_IN_USER } from '@/graphql/queries'
-import { LOGIN_USER, REGISTER_USER } from '@/graphql/mutations'
-
-const AUTH_TOKEN = 'apollo-token'
+import { createStore } from "vuex";
 
 export default createStore({
-  state: {
-    token: null,
-    user: {},
-    authStatus: false
-  },
-  getters: {
-    isAuthenticated: state => !!state.token,
-    authStatus: state => state.authStatus,
-    user: state => state.user
-  },
-  mutations: {
-    SET_TOKEN(state, token) {
-      state.token = token
-    },
-    LOGIN_USER(state, user) {
-      state.authStatus = true
-      state.user = { ...user }
-    },
-    LOGOUT_USER(state) {
-      state.authStatus = ''
-      state.token = '' && localStorage.removeItem(AUTH_TOKEN)
-    }
-  },
-  actions: {
-    async register ({ commit, dispatch }, authDetails) {
-      try {
-        const { data } = await this.$apollo.mutate({ mutation: REGISTER_USER, variables: { ...authDetails } })
-        const token = JSON.stringify(data.register.token)
-        commit('SET_TOKEN', token)
-        // onLogin(apolloClient, user.token)
-        localStorage.setItem(AUTH_TOKEN, token)
-        dispatch('setUser')
-      } catch (e) {
-        console.log(e)
-      }
-    },
-    async login ({ commit, dispatch }, authDetails) {
-      try {
-        const { data } = await this.$apollo.mutate({ mutation: LOGIN_USER, variables: { ...authDetails } })
-        const token = JSON.stringify(data.tokenAuth.token)
-        commit('SET_TOKEN', token)
-        localStorage.setItem(AUTH_TOKEN, token)
-        dispatch('setUser')
-      } catch (e) {
-        console.log(e)
-      }
-    },
-    async setUser ({ commit }) {
-      const { data } = await this.$apollo.query({ query: LOGGED_IN_USER })
-      commit('LOGIN_USER', data.me)
-    },
-  },
-  modules: {
-  }
-})
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+});
